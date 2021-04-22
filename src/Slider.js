e3e93ab140fd6170e4b047f95c39179f997f492f
Shubham Chaudhary <shubhmasand@gmail.com>
2021-04-22 23:03:39 +0530

    Initial commit

---
+++ src/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Slider = () => {
+    return (
+        <div className="slider-container">
+            <input type="range" className="slider"/>
+            Slider
+        </div>
+    )
+}
+
+export default Slider;
